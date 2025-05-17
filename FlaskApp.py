3e8bfe8e041b94af3deed3a2ea044d4037f31576
Abdallah-Fekry <abdallahfekry95@gmail.com>
2025-05-17 20:04:37 +0300

    Update FlaskApp.py

--- FlaskApp.py
+++ FlaskApp.py
@@ -146,5 +146,8 @@ def predict():
     return jsonify({'response':output})
 
 if __name__ == '_main_':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
+    # app.run(debug=True)
 
